bc44016142c843968125146c4d2c3b457acd179e
admin <student@admins-MacBook-Air.local>
2020-05-15 19:41:36 -0700

    able to click add and cancel

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+
+  function transition (newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    history.push(newMode);
+    setMode(newMode);
+    setHistory(history);
+  }
+  function back () {
+    if ( history.length > 1 ) {
+      history.pop();
+      setHistory(history);
+      setMode(history[history.length - 1])
+    } else {
+      setMode(mode);
+    }
+  }
+  return { mode, transition, back };
+}
+
+
